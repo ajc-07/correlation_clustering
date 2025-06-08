5f94d75385095cb31f1ab9dd4f65938afc759bf2
Andrew <57078841+ajc-07@users.noreply.github.com>
2025-06-08 16:41:45 -0700

    Predefined number of clusters
    Simply changed the code to not loop through possible number of clusters but try only a certain number (in our case 10)

--- src/correlation.py
+++ src/correlation.py
@@ -14,103 +14,119 @@ from scipy.optimize import linear_sum_assignment
 import multiprocessing as mp
 
 
-def cluster_correlation_search(G, s = 10, max_attempts = 200, max_iters = 5000, initial = [], split_flag = True):
+def cluster_correlation_search(G, k, max_attempts=200, max_iters=5000, initial=None, split_flag=True):
     """
-    Apply correlation clustering. Assumes that negative edges have weights < 0, and positive edges have weights >= 0, that edges with nan have been removed and that weights are stored under edge attribute G[i][j]['weight'].
+    Apply correlation clustering with exactly k clusters. Assumes that negative edges have weights < 0,
+    and positive edges have weights >= 0, that edges with nan have been removed, and that weights are
+    stored under edge attribute G[i][j]['weight'].
 
     :param G: graph
-    :param s: maximal number of clusters assumed (has strong influence on runtime)
+    :param k: exact number of clusters to find  # CHANGED: replaced 's' parameter with 'k'
     :param max_attempts: number of restarts for optimization
     :param max_iters: number of iterations for optimization
     :param initial: optional clustering for initialization
-    :param split_flag: optional flag, if non evidence cluster should be splitted
-    :return classes, stats: list of clusters, list of stats
+    :param split_flag: optional flag, if non-evidence cluster should be split
+    :return classes, stats: list of clusters, stats dict
     """
- 
-    start_time = time.time()    
+    start_time = time.time()
     stats = {}
     G = G.copy()
 
-    if initial == []: # initialize with connected components unless initial clustering is provided
+    if initial is None or initial == []:
         classes = cluster_connected_components(G)
     else:
         classes = initial
 
-    n2i = {node:i for i, node in enumerate(G.nodes())}
-    i2n = {i:node for i, node in enumerate(G.nodes())}
-    n2c = {n2i[node]:i for i, cluster in enumerate(classes) for node in cluster}
-   
-    edges_positive = set([(n2i[i],n2i[j],G[i][j]['weight']) for (i,j) in G.edges() if G[i][j]['weight'] >= 0.0])
-    edges_negative = set([(n2i[i],n2i[j],G[i][j]['weight']) for (i,j) in G.edges() if G[i][j]['weight'] < 0.0])
-    
-    Linear_loss = Loss('linear_loss', edges_positive=edges_positive, edges_negative=edges_negative)
-    #conflict_loss = test_loss
-    
-    # Define initial state
-    init_state = np.array([n2c[n] for n in sorted(n2c.keys())])
+    # Mapping nodes to indices and back
+    n2i = {node: i for i, node in enumerate(G.nodes())}
+    i2n = {i: node for node, i in n2i.items()}
+    n2c = {n2i[node]: ci for ci, cluster in enumerate(classes) for node in cluster}
+
+    # Separate positive and negative edges
+    edges_positive = set(
+        (n2i[i], n2i[j], G[i][j]['weight'])
+        for (i, j) in G.edges() if G[i][j]['weight'] >= 0.0
+    )
+    edges_negative = set(
+        (n2i[i], n2i[j], G[i][j]['weight'])
+        for (i, j) in G.edges() if G[i][j]['weight'] < 0.0
+    )
+
+    Linear_loss = Loss(
+        'linear_loss', edges_positive=edges_positive, edges_negative=edges_negative
+    )
+
+    # Define initial state array
+    init_state = np.array([n2c.get(n, 0) for n in sorted(n2c.keys())])
     loss_init = Linear_loss.loss(init_state)
 
     if loss_init == 0.0:
-        #print('loss_init: ', loss_init)
-        classes.sort(key=lambda x:-len(x)) # sort by size
+        classes.sort(key=lambda x: -len(x))
         end_time = time.time()
-        stats = stats | {'s':s, 'max_attempts':max_attempts, 'max_iters':max_iters, 'split_flag':split_flag, 'runtime':(end_time - start_time)/60, 'loss':loss_init} 
+        stats = {
+            'k': k,                      # CHANGED: report 'k' instead of 's'
+            'max_attempts': max_attempts,
+            'max_iters': max_iters,
+            'split_flag': split_flag,
+            'runtime': (end_time - start_time)/60,
+            'loss': loss_init
+        }
         return classes, stats
 
-    l2s = defaultdict(lambda: [])
-    l2s[loss_init].append((init_state,len(classes)))
+    # Initialize loss-to-states mapping
+    l2s = defaultdict(list)
+    l2s[loss_init].append((init_state, len(classes)))
 
-    # Initialize multiprocessing.Pool()
+    # Initialize multiprocessing pool
     pool = mp.Pool(mp.cpu_count())
-    #pool = mp.Pool(1)
-    #print(mp.cpu_count())
 
-    # `pool.apply`
-    solutions = pool.starmap(Linear_loss.optimize_simulated_annealing, [(n, classes, G.nodes(), init_state, max_attempts, max_iters) for n in range(2,s)])
-    pool.close()    
-    #print(solutions[0])
-    
-    # Merge solutions
-    for l2s_ in solutions:
-        #print(l2s_)
-        for (l,ss) in l2s_.items():        
-            for st in ss:        
-                l2s[l].append(st)
+    # CHANGED: Only run simulated annealing once for n = k
+    solutions = pool.starmap(
+        Linear_loss.optimize_simulated_annealing,
+        [(k, classes, G.nodes(), init_state, max_attempts, max_iters)]
+    )
+    pool.close()
 
-    #print(l2s.values())
+    # Merge solutions from pool
+    for sol in solutions:
+        for loss_val, states in sol.items():
+            l2s[loss_val].extend(states)
 
-    id = np.random.choice(range(len(l2s[min(l2s.keys())])))
-    best_state, best_fitness = l2s[min(l2s.keys())][id], min(l2s.keys())
-    #print('loss: ', best_fitness)
+    # Select the best state
+    best_loss = min(l2s.keys())
+    chosen_states = l2s[best_loss]
+    best_state, _ = random.choice(chosen_states)
 
-    #print(best_state)
-    best_state = best_state[0]
-    #print(best_state)
-    
-    c2n = defaultdict(lambda: [])
-    for i, c in enumerate(best_state):
-        c2n[c].append(i2n[i])
+    # Map state labels back to node sets
+    c2n = defaultdict(list)
+    for idx_label, comp_label in enumerate(best_state):
+        c2n[comp_label].append(i2n[idx_label])
+    classes = [set(nodes) for nodes in c2n.values()]
 
-    classes = [set(c2n[c]) for c in c2n]
+    if split_flag:
+        classes = split_non_evidence_clusters(G, classes)
 
-    # Split collapsed clusters without evidence
-    if split_flag: classes = split_non_evidence_clusters(G, classes)
-
-    classes.sort(key=lambda x:-len(x)) # sort by size
-
+    classes.sort(key=lambda x: -len(x))
     end_time = time.time()
-    stats = stats | {'s':s, 'max_attempts':max_attempts, 'max_iters':max_iters, 'split_flag':split_flag, 'runtime':(end_time - start_time)/60, 'loss':best_fitness} 
-    
-    #print(stats['runtime'])
-    
+
+    stats = {
+        'k': k,                        # CHANGED: report 'k' instead of 's'
+        'max_attempts': max_attempts,
+        'max_iters': max_iters,
+        'split_flag': split_flag,
+        'runtime': (end_time - start_time)/60,
+        'loss': best_loss
+    }
+
     return classes, stats
+
 
 class Loss(object):
     """
+    Wrapper for different loss computations.
     """
-    
-    def __init__(self, fitness_fn, edges_positive=None, edges_negative=None, edges_min=None, edges_max=None, signs=None):
- 
+    def __init__(self, fitness_fn, edges_positive=None, edges_negative=None,
+                 edges_min=None, edges_max=None, signs=None):
         self.edges_positive = edges_positive
         self.edges_negative = edges_negative
         self.edges_min = edges_min
@@ -125,123 +141,105 @@ class Loss(object):
         if fitness_fn == 'binary_loss_poles':
             self.fitness_fn = self.binary_loss_poles
 
-    def loss(self, state):        
+    def loss(self, state):
         return self.fitness_fn(state)
 
-    def test_loss(self, state):        
+    def test_loss(self, state):
         return 50.0
 
-    def linear_loss(self, state):        
-        loss_pos = np.sum([w for (i,j,w) in self.edges_positive if state[i] != state[j]])
-        loss_neg = np.sum([abs(w) for (i,j,w) in self.edges_negative if state[i] == state[j]])        
-        loss = loss_pos + loss_neg
-        return loss
+    def linear_loss(self, state):
+        return np.sum([w for (i,j,w) in self.edges_positive if state[i]!=state[j]]) \
+             + np.sum([abs(w) for (i,j,w) in self.edges_negative if state[i]==state[j]])
 
-    def binary_loss(self, state):        
-        loss_pos = len([1 for (i,j,w) in self.edges_positive if state[i] != state[j]])
-        loss_neg = len([1 for (i,j,w) in self.edges_negative if state[i] == state[j]])
-        if self.signs==['pos', 'neg']:
-            loss = loss_pos + loss_neg        
-        elif self.signs==['pos']:
-            loss = loss_pos        
-        elif self.signs==['neg']:
-            loss = loss_neg
-        else:
-            loss = float('nan')
-        return loss
+    def binary_loss(self, state):
+        loss_pos = len([1 for (i,j,w) in self.edges_positive if state[i]!=state[j]])
+        loss_neg = len([1 for (i,j,w) in self.edges_negative if state[i]==state[j]])
+        if self.signs==['pos','neg']:
+            return loss_pos + loss_neg
+        if self.signs==['pos']:
+            return loss_pos
+        if self.signs==['neg']:
+            return loss_neg
+        return float('nan')
 
     def binary_loss_poles(self, state):
-        loss_min = len([1 for (i,j,w) in self.edges_min if state[i] == state[j]])
-        loss_max = len([1 for (i,j,w) in self.edges_max if state[i] != state[j]])
-        if self.signs==['min', 'max']:
-            loss = loss_min + loss_max        
-        elif self.signs==['min']:
-            loss = loss_min        
-        elif self.signs==['max']:
-            loss = loss_max
-        else:
-            loss = float('nan')
-        return loss
+        loss_min = len([1 for (i,j) in self.edges_min if state[i]==state[j]])
+        loss_max = len([1 for (i,j) in self.edges_max if state[i]!=state[j]])
+        if self.signs==['min','max']:
+            return loss_min + loss_max
+        if self.signs==['min']:
+            return loss_min
+        if self.signs==['max']:
+            return loss_max
+        return float('nan')
 
     def optimize_simulated_annealing(self, n, classes, nodes, init_state, max_attempts, max_iters):
-
-        # Important to reseed to have different seeds in different pool processes
+        # CHANGED: use n (==k) as the fixed max_val for clustering labels
         np.random.seed()
-        
-        # Initialize custom fitness function object
         fitness_fn = mlrose.CustomFitness(self.fitness_fn)
-
-        l2s_ = defaultdict(lambda: [])
+        l2s_ = defaultdict(list)
 
         # With initial state
-        max_val = max(n,len(classes))
-        problem = mlrose.DiscreteOpt(length = len(nodes), fitness_fn = fitness_fn, maximize = False, max_val = max_val)
-
-        # Define decay schedule
+        problem = mlrose.DiscreteOpt(
+            length=len(nodes), fitness_fn=fitness_fn,
+            maximize=False, max_val=n  # CHANGED: force exactly k clusters
+        )
         schedule = mlrose.ExpDecay()
-        # Solve problem using simulated annealing
-        best_state, best_fitness, fitness_curve = mlrose.simulated_annealing(problem, schedule = schedule, init_state = init_state, max_attempts = max_attempts, max_iters = max_iters)
-        
-        l2s_[best_fitness].append((best_state,max_val))
+        best_state, best_fitness, _ = mlrose.simulated_annealing(
+            problem, schedule=schedule, init_state=init_state,
+            max_attempts=max_attempts, max_iters=max_iters
+        )
+        l2s_[best_fitness].append((best_state, n))
 
-        # Important to reseed to have different seeds in different pool processes
         np.random.seed()
-        
         # Repeat without initial state
-        max_val = n
-        problem = mlrose.DiscreteOpt(length = len(nodes), fitness_fn = fitness_fn, maximize = False, max_val = max_val)
-
+        problem = mlrose.DiscreteOpt(
+            length=len(nodes), fitness_fn=fitness_fn,
+            maximize=False, max_val=n  # CHANGED: force exactly k clusters
+        )
         schedule = mlrose.ExpDecay()
-        best_state, best_fitness, fitness_curve = mlrose.simulated_annealing(problem, schedule = schedule, max_attempts = max_attempts, max_iters = max_iters)
-        l2s_[best_fitness].append((best_state,max_val))
+        best_state2, best_fitness2, _ = mlrose.simulated_annealing(
+            problem, schedule=schedule,
+            max_attempts=max_attempts, max_iters=max_iters
+        )
+        l2s_[best_fitness2].append((best_state2, n))
 
         return dict(l2s_)
 
-    
+
 def cluster_connected_components(G, is_non_value=lambda x: np.isnan(x)):
     """
-    Apply connected_component clustering.       
-    :param G: graph
-    :return classes: list of clusters
+    Apply connected component clustering on positive edges.
     """
-
     G = G.copy()
-
-    edges_negative = [(i,j) for (i,j) in G.edges() if G[i][j]['weight'] < 0.0 or is_non_value(G[i][j]['weight'])]
+    edges_negative = [
+        (i,j) for (i,j) in G.edges()
+        if G[i][j]['weight'] < 0.0 or is_non_value(G[i][j]['weight'])
+    ]
     G.remove_edges_from(edges_negative)
     components = nx.connected_components(G)
-    classes = [set(component) for component in components]
-    classes.sort(key=lambda x:list(x)[0])
-
+    classes = [set(c) for c in components]
+    classes.sort(key=lambda x: list(x)[0])
     return classes
 
 
 def split_non_evidence_clusters(G, clusters, is_non_value=lambda x: np.isnan(x)):
     """
-    Split non-positively-connected components.       
-    :param G: graph
-    :param clusters: list of clusters
-    :return G: 
+    Split clusters by removing negative edges and reconnected components.
     """
-
     G = G.copy()
-    
-    nodes_in = [node for cluster in clusters for node in cluster]
-    edges_negative = [(i,j) for (i,j) in G.edges() if G[i][j]['weight'] < 0.0 or is_non_value(G[i][j]['weight'])]
-    G.remove_edges_from(edges_negative) # treat non-edges as non-comparisons
-
+    nodes_in = [node for c in clusters for node in c]
+    edges_negative = [
+        (i,j) for (i,j) in G.edges()
+        if G[i][j]['weight'] < 0.0 or is_non_value(G[i][j]['weight'])
+    ]
+    G.remove_edges_from(edges_negative)
     classes_out = []
     for cluster in clusters:
-        subgraph = G.subgraph(cluster)
-        components = cluster_connected_components(subgraph)
-        for class_ in components:
-            classes_out.append(set(class_))
-     
-    # check that nodes stayed the same
-    nodes_out = [node for class_ in classes_out for node in class_]
-    if set(nodes_in) != set(nodes_out):
-        sys.exit('Breaking: nodes_in != nodes_out.')
-    if len(nodes_in) != len(nodes_out):
-        sys.exit('Breaking: len(nodes_in) != len(nodes_out).')
-    
+        sub = G.subgraph(cluster)
+        for comp in nx.connected_components(sub):
+            classes_out.append(set(comp))
+    nodes_out = [n for c in classes_out for n in c]
+    if set(nodes_in) != set(nodes_out) or len(nodes_in) != len(nodes_out):
+        sys.exit('Breaking: node mismatch after split.')
     return classes_out
